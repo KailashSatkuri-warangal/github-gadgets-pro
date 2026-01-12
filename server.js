1f32b73728a48ebd5cfef5926e880966219cdf97
KailashSatkuri-warangal <satkurikailash@gmail.com>
2026-01-12 20:13:19 +0530

    Add Vercel serverless entry (start.js)

--- server.js
+++ server.js
@@ -3,4 +3,4 @@
 import dotenv from "dotenv";
 dotenv.config({ override: true }); // Make sure env is injected globally
 
-import "./api/index"; // Start the app after env is loaded
+import "./start.js"; // Start the app after env is loaded
